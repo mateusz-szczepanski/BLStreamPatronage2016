c96db7f0a4c2040d766b4222b6280be525a9eed7
mateusz-szczepanski <mateusz.szczepanski.1992@gmail.com>
2016-01-23 17:56:31 +0100

    Zadanie 1

---
+++ Patronat/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Search;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Patronat
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadImage();
+        }
+
+        public async void LoadImage()
+        {
+            List<string> fileTypeFilter = new List<string>();
+            fileTypeFilter.Add(".jpg");
+            fileTypeFilter.Add(".png");
+            fileTypeFilter.Add(".bmp");
+            fileTypeFilter.Add(".gif");
+
+            var queryOptions = new QueryOptions(CommonFileQuery.OrderByName, fileTypeFilter);
+
+            try
+            {
+                var query = KnownFolders.PicturesLibrary.CreateFileQueryWithOptions(queryOptions);
+                IReadOnlyList<StorageFile> fileList = await query.GetFilesAsync();
+
+                if (fileList.Count > 0)
+                {
+                    using (IRandomAccessStream fileStream = await fileList.First().OpenAsync(Windows.Storage.FileAccessMode.Read))
+                    {
+                        BitmapImage bitmapImage = new BitmapImage();
+                        await bitmapImage.SetSourceAsync(fileStream);
+                        imageControl.Source = bitmapImage;
+                    }
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Debug.WriteLine("The app was denied access to the PicturesLibrary");
+            }
+
+        }
+    }
+}
